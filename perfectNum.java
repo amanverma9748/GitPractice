e7e6bdac73c73ffdad81e29f5b602765e7c60d69
unknown <amanverma9748@gmail.com>
2025-07-08 11:12:30 +0530

    perfect number check

---
+++ perfectNum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class takeuforward {
+
+  static boolean isPerfect(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n - 1; i++) {
+      if (n % i == 0)
+        sum = sum + i;
+    }
+    if (sum == n)
+        return true;
+    else 
+        return false;
+  }
+
+  public static void main(String[] args) {
+
+    boolean ex1 = isPerfect(6);
+
+    if (ex1 == true) {
+      System.out.println("6 is a perfect Number");
+    } else System.out.println("6 is a perfect Number");
+}
+}
