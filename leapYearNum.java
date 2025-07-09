55b37c75db10e91dbd6f518dee746d96ff7e19cc
unknown <amanverma9748@gmail.com>
2025-07-09 09:34:41 +0530

    Check leap year

---
+++ leapYearNum.java
@@ -0,0 +1,12 @@
+public class leapYearNum 
+{
+  public static void main(String args[]) {
+    int year = 2024;
+    if (((year % 4 == 0) && (year % 100 != 0)) ||(year % 400 == 0)) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
+  }
+}
+    
