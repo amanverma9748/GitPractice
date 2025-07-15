7fa266fe92c5336dbad45ef79aa394c927cc4341
unknown <amanverma9748@gmail.com>
2025-07-15 22:31:45 +0530

    Day 9

---
+++ concatString.java
@@ -0,0 +1,13 @@
+  
+class concatString {
+  
+    public static void main(String[] args)
+    {
+        String str1 = "Hello";
+        String str2 = "World";
+        
+        str1 = str1+ str2;
+        
+        System.out.print(str1);
+    }
+}
