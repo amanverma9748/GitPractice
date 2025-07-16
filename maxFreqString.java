84d18dc7c40a9cf3f6f3691152b026ce9b3707b5
unknown <amanverma9748@gmail.com>
2025-07-16 11:14:36 +0530

    Day 10

---
+++ maxFreqString.java
@@ -0,0 +1,20 @@
+public class maxFreqString
+ {
+   static char maxOccurringChar(String str) {
+      char ans = 'a';
+      int maxfreq = 0, n = str.length();
+      int count[] = new int[256];
+      for (int i = 0; i < n; i++) {
+         count[str.charAt(i)]++;
+         if (count[str.charAt(i)] > maxfreq) {
+            maxfreq = count[str.charAt(i)];
+            ans = str.charAt(i);
+         }
+      }
+      return ans;
+   }
+   public static void main(String[] args) {
+      String str = "Amanmmmmm";
+      System.out.println("Maximum occurring character is " + maxOccurringChar(str));
+   }
+} 
